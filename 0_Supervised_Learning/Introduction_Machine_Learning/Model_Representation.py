722d1e4971cf5719320cd551b51e35404320c3e7
kyomangold <91421645+kyomangold@users.noreply.github.com>
2024-01-24 20:21:59 +0100

    added code files for introduction to ML

---
+++ 0_Supervised_Learning/Introduction_Machine_Learning/Model_Representation.py
@@ -0,0 +1,97 @@
+# Implement the model f_{w,b} for linear regression with one variable
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+
+# # Problem Statement
+# Housing price prediction.  
+# This lab will use a simple data set with only two data points - a house with 1000 square feet(sqft) sold for \\$300,000 and a house with 2000 square feet sold for \\$500,000. These two points will constitute our *data or training set*. In this lab, the units of size are 1000 sqft and the units of price are 1000s of dollars.
+# 
+# | Size (1000 sqft)     | Price (1000s of dollars) |
+# | -------------------| ------------------------ |
+# | 1.0               | 300                      |
+# | 2.0               | 500                      |
+# 
+
+# x_train is the input variable (size in 1000 square feet)
+# y_train is the target (price in 1000s of dollars)
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300.0, 500.0])
+print(f"x_train = {x_train}")
+print(f"y_train = {y_train}")
+
+# m is the number of training examples
+print(f"x_train.shape: {x_train.shape}")
+m = x_train.shape[0]
+print(f"Number of training examples is: {m}")
+
+# m is the number of training examples
+m = len(x_train)
+print(f"Number of training examples is: {m}")
+
+i = 0 # Change this to 1 to see (x^1, y^1)
+
+x_i = x_train[i]
+y_i = y_train[i]
+print(f"(x^({i}), y^({i})) = ({x_i}, {y_i})")
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r')
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.show()
+
+# Adjusting w and b
+w = 100
+b = 100
+print(f"w: {w}")
+print(f"b: {b}")
+
+def compute_model_output(x, w, b):
+    """
+    Computes the prediction of a linear model
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      w,b (scalar)    : model parameters  
+    Returns
+      f_wb (ndarray (m,)): model prediction
+    """
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+        
+    return f_wb
+
+tmp_f_wb = compute_model_output(x_train, w, b,)
+
+# Plot model prediction
+plt.plot(x_train, tmp_f_wb, c='b',label='Prediction')
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.legend()
+plt.show()
+
+# Adjusting w and b
+w = 200                         
+b = 100    
+x_i = 1.2
+cost_1200sqft = w * x_i + b    
+
+print(f"${cost_1200sqft:.0f} thousand dollars")
+
+
+
